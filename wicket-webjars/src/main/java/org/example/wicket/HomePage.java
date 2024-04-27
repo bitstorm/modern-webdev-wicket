7818b0d3323c6e0cdbd7a869c4d94377460da065
Andrea Del Bene <adelbene@apache.org>
2024-04-27 19:47:40 +0200

    using webjars via code

--- wicket-webjars/src/main/java/org/example/wicket/HomePage.java
+++ wicket-webjars/src/main/java/org/example/wicket/HomePage.java
@@ -1,13 +1,25 @@
 package org.example.wicket;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import de.agilecoders.wicket.webjars.request.resource.WebjarsCssResourceReference;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
+	}
+	
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+
+		response.render(CssHeaderItem.forReference(
+                new WebjarsCssResourceReference("bootstrap/current/css/bootstrap.min.css")));
+
 	}
 }
